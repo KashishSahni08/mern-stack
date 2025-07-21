c3e07b0c0e5a1abf7e159d3cac3c19d56c19aa0e
KashishSahni <kashishsahni54@gmail.com>
2025-07-21 12:31:14 +0530

    index.js

---
+++ Section1/javascript/index.js
@@ -0,0 +1,24 @@
+console.log("Hello world javascript");
+
+num = 5624;
+
+console.log(num);
+
+console.log (typeof num);
+
+msg ="Good Morning";
+
+console.log(msg);
+
+console.log(typeof msg);
+
+console.log(2 ** 5);
+
+console.log(1+'1');
+console.log(1+1+'1');
+console.log(1+ +'a');
+console.log( 2 + undefined + 'a');
+
+console.log(1 ==1);
+console.log(1+1+'1');
+console.log(1+1+'1');
