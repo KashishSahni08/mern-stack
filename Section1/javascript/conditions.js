f8b2295e517f58b3fa33ee77ec9447a4fbbb2ce5
KashishSahni <kashishsahni54@gmail.com>
2025-07-21 12:32:03 +0530

    conditions.js

---
+++ Section1/javascript/conditions.js
@@ -0,0 +1,35 @@
+num = 42;
+
+if (num % 2 === 0){
+
+   let msg = 'nice';
+   const pi = 3.14;
+//    pi = 3.412;
+
+    console.log('even');   
+}else{
+    console.log('add');
+};
+//  console.log(msg);
+// console.log(pi);
+
+const age = 20;
+//Wap to check if user is eligible for dl
+
+if (age >= 18){
+    console.log('eligible for DL');
+} else{
+ console.log(' Not eligible for DL');
+};
+
+const marks =80;
+
+if (marks<=100 && marks >=90){
+    console.log('Grade A');
+}else if (marks < 90 && marks >= 70 ) {
+        console.log('Grade B');
+}else if (marks < 70 && marks >= 40 ) {
+        console.log('Grade C');
+} else{
+    console.log('Failed');
+}
